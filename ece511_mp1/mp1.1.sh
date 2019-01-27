422f46ac8942f65bc0187b96713492c77ee8d8d7
Andrew <atsmith3@illinois.edu>
2019-01-27 17:40:27 -0600

    baseline setpup complete

--- ece511_mp1/mp1.1.sh
+++ ece511_mp1/mp1.1.sh
@@ -2,9 +2,11 @@
 
 # Directories
 GEM5_DIR='/home/andrew/ece511/gem5'
-GEM5_BUILD_DIR='build/ARM_MSI'
+GEM5_BUILD_DIR='build/ARM'
 MP_DIR='ece511/ece511_mp1'
 CONF_DIR='configs/example'
+
+BINARY_ARGS='-m16 -p2'
 
 # Number of CPU's
 NUM_CPUS=2
@@ -21,9 +23,9 @@ NUM_L2CACHES=$NUM_CPUS
 L2_ASSOC=8
 
 # L3 Cache Settings
-L3_SIZE='1MB'
-NUM_L3CACHES=$NUM_CPUS
-L3_ASSOC=8
+#L3_SIZE='1MB'
+#NUM_L3CACHES=$NUM_CPUS
+#L3_ASSOC=8
 
 
 #$GEM5_DIR/$GEM5_BUILD_DIR/gem5.opt \
@@ -38,5 +40,14 @@ $GEM5_DIR/$GEM5_BUILD_DIR/gem5.opt \
 --json-config=mp1.1.json \
 $GEM5_DIR/$CONF_DIR/se.py \
 --cmd=$GEM5_DIR/$MP_DIR/fft \
+--options="$BINARY_ARGS" \
 --cpu-type=TimingSimpleCPU \
-# Caches
+--l1i_size=$L1I_SIZE \
+--l1i_assoc=$L1I_ASSOC \
+--l1d_size=$L1D_SIZE \
+--l1d_assoc=$L1D_ASSOC \
+--l2cache \
+--l2_size=$L2_SIZE \
+--l2_assoc=$L2_ASSOC \
+--num-l2caches=$NUM_L2CACHES \
+--ruby 
